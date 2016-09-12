1224155ac9f182d88d9ade178c00a4b34d6bca82
Agustin Borgna <hello@aborgna.com.ar>
2016-09-12 00:56:17 -0300

    ej1

--- tp2/src/ej1.cpp
+++ tp2/src/ej1.cpp
@@ -13,23 +13,128 @@
 
 using namespace std;
 
+typedef struct point_t {
+    int64_t x;
+    int64_t y;
+
+    bool operator==(const struct point_t &o) const {
+        return tie(x, y) == tie(o.x, o.y);
+    }
+} point;
+
+int64_t n, m, pmax;
+vector<vector<bool>> mapa;  // True == pared
+point origen;
+point destino;
+
+void agregarCandidato(queue<pair<point, int64_t>> &candidatos,
+                      vector<vector<vector<int64_t>>> &dist, point v, int64_t p,
+                      int64_t d);
+
+int64_t bfs() {
+    vector<vector<vector<int64_t>>> dist(
+        m, vector<vector<int64_t>>(n, vector<int64_t>(pmax + 1, -1)));
+
+    queue<pair<point, int64_t>> candidatos;
+
+    // Inicializar el primer lugar
+    dist[origen.y][origen.x][0] = 0;
+    candidatos.emplace(origen, 0);
+
+    while (!candidatos.empty()) {
+        auto cand = candidatos.front();
+        candidatos.pop();
+        point u = cand.first;
+        int64_t p = cand.second;
+        int64_t d = dist[u.y][u.x][p];
+
+        if (u == destino) {
+            return d;
+        }
+
+        // Agregamos los vecinos, si no estan visitados
+        agregarCandidato(candidatos, dist, {u.x - 1, u.y}, p, d);
+        agregarCandidato(candidatos, dist, {u.x + 1, u.y}, p, d);
+        agregarCandidato(candidatos, dist, {u.x, u.y - 1}, p, d);
+        agregarCandidato(candidatos, dist, {u.x, u.y + 1}, p, d);
+    }
+
+    return -1;
+}
+
+void agregarCandidato(queue<pair<point, int64_t>> &candidatos,
+                      vector<vector<vector<int64_t>>> &dist, point v, int64_t p,
+                      int64_t d) {
+    // Nunca agregamos los bordes del mapa
+    if (v.x <= 0 or v.y <= 0 or v.x >= n - 1 or v.y >= m - 1)
+        return;
+
+    // Si hay una pared en v nos cuesta una demolicion mas llegar
+    if (mapa[v.y][v.x]) {
+        p++;
+        if (p > pmax)
+            return;
+    }
+
+    // Si ya está visitado no lo agregamos
+    if (dist[v.y][v.x][p] != -1)
+        return;
+
+    // Lo agregamos a la cola
+    candidatos.emplace(v, p);
+    dist[v.y][v.x][p] = d + 1;
+}
+
 /**************************************
  * Funciones del framework de testeo
  **************************************/
 
-void prob_load(std::istream &is) {}
+void prob_load(std::istream &is) {
+    is >> m >> n >> pmax;
 
-int prob_solve(std::ostream &os) {}
+    mapa = vector<vector<bool>>(m, vector<bool>(n, false));
+
+    for (int64_t y = 0; y < m; y++) {
+        for (int64_t x = 0; x < n; x++) {
+            char celda;
+            is >> celda;
+
+            switch (celda) {
+                case 'o':
+                    origen = {x, y};
+                    break;
+                case 'x':
+                    destino = {x, y};
+                    break;
+                case '#':
+                    mapa[y][x] = true;
+                    break;
+            }
+        }
+    }
+}
+
+int prob_solve(std::ostream &os) {
+    int64_t res = bfs();
+
+    os << res << endl;
+
+    return res >= 0 ? 0 : -1;
+}
 
 void prob_reload() {}
 
-vector<uint64_t> prob_vars() {}
+vector<uint64_t> prob_vars() {
+    return {(uint64_t)n, (uint64_t)m, (uint64_t)pmax};
+}
 
-void prob_print_input(std::ostream &os) {}
+void prob_print_input(std::ostream &os) {
+    for (auto v : prob_vars()) os << v << " ";
+    os << endl;
+}
 
 vector<Option> prob_custom_options() { return {}; }
 
 void generator_random(const std::vector<uint64_t> &v) {}
 
 vector<Generator> prob_generators() { return {}; }
-
