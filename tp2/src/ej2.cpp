6a1c2c266bf01890ec28a873abf17ad1678b089c
alan-corleto <alancorleto@hotmail.es>
2016-09-16 21:00:12 -0300

    Ej3 terminadooooooooooooo

--- tp2/src/ej2.cpp
+++ tp2/src/ej2.cpp
@@ -18,6 +18,121 @@ int64_t n, m;
 vector<vector<int64_t>>
     mapa;  // -1 = pared dura, 0 = sala, >0 = pared destruible
 
+typedef struct point_t {
+    int64_t x;
+    int64_t y;
+
+    bool operator==(const struct point_t &o) const {
+        return tie(x, y) == tie(o.x, o.y);
+    }
+    bool operator!=(const struct point_t &o) const {
+        return tie(x, y) != tie(o.x, o.y);
+    }
+    bool operator<(const struct point_t &o) const {
+        return tie(x, y) < tie(o.x, o.y);
+    }
+} point;
+
+typedef struct arista_t {
+    point p;
+    point q;
+    int64_t peso;
+
+    bool operator==(const struct arista_t &o) const {
+        return tie(p, q, peso) == tie(o.p, o.q, o.peso) or
+               tie(p, q, peso) == tie(o.q, o.p, o.peso) ;
+    }
+    bool operator<(const struct arista_t &o) const {
+        return tie(peso, p, q) < tie(o.peso, o.p, o.q);
+    }
+} arista;
+
+vector<vector<int64_t>> altura_uf;
+vector<vector<point>> padre_uf;
+
+void init_uf(){
+    altura_uf = vector<vector<int64_t>>(m,vector<int64_t>(n,1));
+
+    padre_uf.resize(m);
+    for(int64_t y = 0; y < m ; y++) {
+        padre_uf[y].resize(n);
+        for(int64_t x = 0; x < n ; x++){
+            padre_uf[y][x] = {x,y};
+        }
+    }
+}
+
+point find_uf(point p){
+    if (padre_uf[p.y][p.x] != p)
+        padre_uf[p.y][p.x] = find_uf(padre_uf[p.y][p.x]);
+    return padre_uf[p.y][p.x];
+}
+
+void union_uf(point p, point q){
+    p = find_uf(p);
+    q = find_uf(q);
+    if (altura_uf[p.y][p.x] < altura_uf[q.y][q.x])
+        padre_uf[p.y][p.x] = q;
+    else
+        padre_uf[q.y][q.x] = p;
+    if (altura_uf[p.y][p.x] == altura_uf[q.y][q.x])
+        altura_uf[p.y][p.x]++;
+}
+
+int64_t kruskal(){
+    init_uf();
+    set<arista> candidatos;
+    int64_t res = 0;
+    int64_t componentes_conexas = 0;
+
+    //Agregar las aristas
+    for (int64_t y = 1 ; y < m - 1 ; y++){
+        for (int64_t x = 1 ; x < n - 1 ; x++){
+            if (mapa[y][x] == 0){
+                if (mapa[y][x+1] == 0)
+                    candidatos.insert({{x,y}, {x+1,y}, 0});
+                if (mapa[y+1][x] == 0)
+                    candidatos.insert({{x,y}, {x,y+1}, 0});
+                componentes_conexas++;
+            }
+            if (mapa[y][x] > 0){
+                point p ={0,0} ,q;
+                if(mapa[y-1][x] == 0) {
+                    q = p;
+                    p = {x,y-1};
+                } 
+                if(mapa[y+1][x] == 0) {
+                    q = p;
+                    p = {x,y+1};
+                } 
+                if(mapa[y][x-1] == 0) {
+                    q = p;
+                    p = {x-1,y};
+                } 
+                if(mapa[y][x+1] == 0) {
+                    q = p;
+                    p = {x+1,y};
+                }
+                candidatos.insert({p, q, mapa[y][x]});
+            }
+        }
+    }
+
+    //Hacer Kruskal
+    for(auto &ar : candidatos) {
+        if (find_uf(ar.p) == find_uf(ar.q))
+            continue;
+        union_uf(ar.p, ar.q);
+        res += ar.peso;
+        componentes_conexas--;
+
+        if(componentes_conexas == 1)
+            break;
+    }
+
+    return componentes_conexas == 1 ? res : -1;
+}
+
 /**************************************
  * Funciones del framework de testeo
  **************************************/
@@ -48,9 +163,7 @@ void prob_load(std::istream &is) {
 }
 
 int prob_solve(std::ostream &os) {
-    int64_t res = 0;
-
-    // Todo: resolver
+    int64_t res = kruskal();
 
     os << res << endl;
 
@@ -80,11 +193,11 @@ void prob_print_input(std::ostream &os) {
 
 vector<Option> prob_custom_options() { return {}; }
 
-void generator_random(const std::vector<uint64_t> &v) {
+void generator_conexo(const std::vector<uint64_t> &v) {
     m = v[0];
     n = v[1];
 
-    generateDungeon<int64_t>(m, n, 0.6, mapa, -1, 0);
+    generateDungeon<int64_t>(m, n, 0.7, mapa, -1, 0);
 
     // Asignamos valores a las paredes
     for (int64_t y = 1; y < m - 1; y++) {
@@ -136,4 +249,17 @@ void generator_random(const std::vector<uint64_t> &v) {
     }
 }
 
-vector<Generator> prob_generators() { return {{"random", &generator_random}}; }
+void generator_no_conexo(const std::vector<uint64_t> &v) {
+    generator_conexo(v);
+
+    // Asignamos valores a las paredes
+    for (int64_t y = 1; y < m - 1; y++)
+        for (int64_t x = 1; x < n - 1; x++)
+            if (mapa[y][x] > 0 and rnd(0,1))
+                mapa[y][x] = -1;
+}
+
+vector<Generator> prob_generators() {
+    return {{"conexo", &generator_conexo},
+            {"no_conexo", &generator_no_conexo}};
+}
